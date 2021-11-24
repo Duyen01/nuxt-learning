a2d97f8d3671c153d9d6e04c7705a223f4e0b6d4
duyenpm <duyenpm@runsystem.net>
2021-11-24 09:26:29 +0700

    push to fix get data from firebase

--- pages/decks/index.vue
+++ pages/decks/index.vue
@@ -38,44 +38,27 @@ export default {
     DeckList,
     DeckForm
   },
-  asyncData(context) {
-    return new Promise((resolve, reject) => {
-      // eslint-disable-next-line nuxt/no-timing-in-fetch-data
-    setTimeout(() => {
-      resolve({
-        decks: [
-        {
-          _id: 1,
-          name: 'Learn English',
-          description: 'Fake dữ liệu chuẩn bị kết nối API trong dự án Nuxt.js - RHP Team',
-          thumbnail: 'https://wallpaperbat.com/img/566655-english-class-wallpaper-png-free-english-class-wallpaperpng-transparent-image.png'
-        },
-        {
-          _id: 2,
-          name: 'Learn JavaScript',
-          description: 'Fake dữ liệu chuẩn bị kết nối API trong dự án Nuxt.js - RHP Team',
-          thumbnail: 'https://wallpaperbat.com/img/566655-english-class-wallpaper-png-free-english-class-wallpaperpng-transparent-image.png'
-        },
-        {
-          _id: 3,
-          name: 'Learn NuxtJs',
-          description: 'Fake dữ liệu chuẩn bị kết nối API trong dự án Nuxt.js - RHP Team',
-          thumbnail: 'https://wallpaperbat.com/img/566655-english-class-wallpaper-png-free-english-class-wallpaperpng-transparent-image.png'
-        },
-      ]
+   asyncData (context) {
+    return axios
+      .get('https://nuxt-learning-demo-default-rtdb.firebaseio.com/decks.json')
+      .then((response) => {
+        return {
+            decks: response.data
+        } 
+              
       })
-    }, 1000)
-    }).then((data) => {
-      return data
-    }).catch((e) => {
-      // eslint-disable-next-line unicorn/error-message
-      context.error(e)
-    })
+      .catch((e) => {
+        context.error(e)
+      })       
+  },
+  data(){
+    return {
+      decks: []
+    }
   },
   create() {
+    
     this.$store.dispatch('setDecks', this.decks)
-    // eslint-disable-next-line no-console
-    console.log(this.$store.getters.decks)
   },
   methods: {
     openModal() {
@@ -85,23 +68,17 @@ export default {
       this.$modal.close({ name: 'createDeckModal' })
     },
     onSubmit(deckData) {
-      axios.post('https://nuxt-learning-2866a-default-rtdb.firebaseio.com/decks.json', deckData)
-      .then((data) => {
+      axios.post('https://nuxt-learning-demo-default-rtdb.firebaseio.com/decks.json', deckData)
+      .then((response) => {
+        console.log(response);
         // eslint-disable-next-line no-console
-        console.log(data)
+          this.decks.push({ id: response.data.name})    
       })
       .catch((e) => {
         // eslint-disable-next-line no-console
         console.log(e)
       })
     },
-    // eslint-disable-next-line require-await
-    async getData() {
-      const messageRef = this.$fireDb.ref('cases') // Where 'cases' is the json object
-      axios.get(messageRef.toString() + '.json').then(response => {
-        console.log(response.data)
-      })
-    }
   },
 }
 </script>
